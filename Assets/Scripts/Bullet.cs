bc595b5e767083592cb11145b92d04df64b3a8f7
damdam6680 <damdam6680@gmail.com>
2024-11-19 22:19:50 +0100

    strzelanie 2

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,13 +4,38 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision objectWeHit)
     {
 
-        if (collision.gameObject.CompareTag("Target"))
+        if (objectWeHit.gameObject.CompareTag("Target"))
         {
-            print("Hit Target: " + collision.gameObject.name);
+            print("Hit Target: " + objectWeHit.gameObject.name);
+
+            CreateBulletImpactEffect(objectWeHit);
+
             Destroy(gameObject);
         }
+
+        if (objectWeHit.gameObject.CompareTag("Wall"))
+        {
+            print("Hit Wall");
+
+            CreateBulletImpactEffect(objectWeHit);
+
+            Destroy(gameObject);
+        }
+    }
+
+    void CreateBulletImpactEffect(Collision objectWeHit)
+    {
+        ContactPoint contact = objectWeHit.contacts[0];
+
+        GameObject hole = Instantiate(
+             GlobalReferences.Instance.bulletImpactEffectPrefb,
+             contact.point,
+             Quaternion.LookRotation(contact.normal)
+            ) ;
+
+        hole.transform.SetParent(objectWeHit.gameObject.transform);
     }
 }
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -6,33 +6,119 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
 
+
+    //Strzelanie
+    public bool isShooting, readyToShoot;
+    bool allowReset = true;
+    public float shootingDelay = 0.5f;
+
+    //Burst
+    public int bulletsPerBurst = 3;
+    public int burstBulletsLeft;
+
+    //Spred
+    public float speadIntensity;
+
+    //Bullet
     public GameObject bulletPrefab;
     public Transform bulletSpawn;
     public float bulletVelocity = 30;
     public float bulletPrefabLifeTime = 3f;
-    // Start is called before the first frame update
-    void Start()
+
+    public enum ShootingMode
     {
-        
+        Single,
+        Burst,
+        Auto
+    }
+
+    public ShootingMode currentShootingMode;
+
+    private void Awake()
+    {
+        readyToShoot = true;
+        burstBulletsLeft = bulletsPerBurst;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+
+        if (currentShootingMode == ShootingMode.Auto)
         {
+            isShooting = Input.GetKey(KeyCode.Mouse0);
+
+        }
+        else if (currentShootingMode == ShootingMode.Single || currentShootingMode == ShootingMode.Burst)
+        {
+            isShooting = (Input.GetKeyDown(KeyCode.Mouse0));
+        }
+
+        if (readyToShoot && isShooting)
+        {
+            burstBulletsLeft = bulletsPerBurst;
             FireWeapon();
         }
+
     }
 
     private void FireWeapon()
     {
+        readyToShoot = false;
+
+        Vector3 shootingDirection = CalculateDirectionAndSpread();
 
         GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, Quaternion.identity);
 
-        bullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.forward.normalized * bulletVelocity, ForceMode.Impulse);
+        bullet.transform.forward = shootingDirection;
+
+        bullet.GetComponent<Rigidbody>().AddForce(shootingDirection * bulletVelocity, ForceMode.Impulse);
 
         StartCoroutine(DestroyBulletAfterTime(bullet, bulletPrefabLifeTime));
+
+        if (allowReset)
+        {
+            Invoke("ResetShoot", shootingDelay);
+            allowReset = false;
+        }
+
+        if (currentShootingMode == ShootingMode.Burst && burstBulletsLeft > 1)
+        {
+            burstBulletsLeft--;
+            Invoke("FireWeapon", shootingDelay);
+        }
+    }
+
+    private void ResetShoot()
+    {
+        readyToShoot = true;
+        allowReset = true;
+    }
+
+
+    private Vector3 CalculateDirectionAndSpread()
+    {
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+
+        Vector3 targetPoint;
+        if (Physics.Raycast(ray, out hit))
+        {
+            targetPoint = hit.point;
+        }
+        else
+        {
+            targetPoint = ray.GetPoint(100); 
+        }
+
+        Vector3 direction = targetPoint - bulletSpawn.position;
+
+        // Add spread
+        float x = UnityEngine.Random.Range(-speadIntensity, speadIntensity);
+        float y = UnityEngine.Random.Range(-speadIntensity, speadIntensity);
+        Debug.Log($"Direction: {direction}, Spread X: {x}, Spread Y: {y}");
+        direction += new Vector3(x, y, 0);
+        return direction.normalized;
     }
 
     private IEnumerator DestroyBulletAfterTime(GameObject bullet, float bulletPrefabLifeTime)
